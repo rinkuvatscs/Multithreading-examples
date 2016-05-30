7160c242ad3940e82a482c708c8e307a2befe878
Rinku Sharma <rsharma3@xavient.com>
2016-05-30 14:48:16 +0530

    Intail commit

---
+++ src/main/java/com/multithreading/MultithreadingExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingExamplesApplication.class, args);
+	}
+}
